bf04e69381a1880545711b8ad07309814a53de8b
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-11-02 13:58:37 -0500

    finally made some meaningful changes to css. the about me section looks a lot better

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,6 +1,10 @@
 
+// need to make nav bar links clickable and use show() hide() functions with on click listeners
+// need to make project pics with an on click listener and take them to the projects page for that specific project and expand the description
 
+// need to make descriptions (p) show/hide. Toggle???
 
+//need to make hide / show for project descriptions
 
 
 
